8360501304e6d8912c35fa9d8979b8c49add6fa3
Alex Raskin <alexraskin@fastmail.fm>
2025-03-29 20:42:25 +0000

    add log

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log/slog"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -9,6 +10,7 @@ import (
 func main() {
 	r := chi.NewRouter()
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("Request received", "method", r.Method, "url", r.URL.String(), "ip", r.RemoteAddr)
 		w.Write([]byte("Hello, World!"))
 	})
 	http.ListenAndServe(":8080", r)
